0c987db8695e14b12ced4090e95cc813631df917
vincent mohanna <vince.mohanna@gmail.com>
2026-02-26 18:10:58 +0100

    Got some images and movies displayed

--- Gemfile
+++ Gemfile
@@ -8,7 +8,11 @@ gem "rails", "~> 8.1.2"
 # ADD these gems:
 gem "sprockets-rails"
 # gem "sassc-rails"
+gem 'sass-embedded', '~> 1.0'
 gem "dartsass-rails"
+# Add this to your Gemfile
+gem "dartsass-sprockets"
+
 gem "bootstrap", "~> 5.3"
 gem "autoprefixer-rails"
 # gem "font-awesome-sass", "~> 6.1"
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "movies#index"
+
+  # get "movies", to: "movies#index"
+  resources :movies do
+    resources :lists
+  end
 end
